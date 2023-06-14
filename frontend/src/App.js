4239d5a86024ffca02217cf80b9ec3a50bd0e154
Raghav Awasthi <raghavawasthi240@gmail.com>
2023-06-15 02:12:10 +0530

    home page created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
-
+import {Routes,Route} from "react-router-dom";
+import Home from "./pages/Home/Home";
 
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+    </Routes>
   );
 }
 
